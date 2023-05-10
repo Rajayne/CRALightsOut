797d95cc90b4a3b60237819bf6459c4c0c657147
Rajayne <ryann.j.nealey@gmail.com>
2023-05-10 16:36:47 -0700

    Finish createBoard function to generate matrix nrows x ncols

--- src/Board.js
+++ src/Board.js
@@ -3,29 +3,21 @@ import Cell from "./Cell";
 import "./Board.css";
 
 /** Game board of Lights out.
- *
  * Properties:
- *
  * - nrows: number of rows of board
  * - ncols: number of cols of board
  * - chanceLightStartsOn: float, chance any cell is lit at start of game
  *
  * State:
- *
  * - board: array-of-arrays of true/false
- *
  *    For this board:
  *       .  .  .
  *       O  O  .     (where . is off, and O is on)
  *       .  .  .
- *
  *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
+
  *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
+ *  This doesn't handle any clicks --- clicks are on individual cells **/
 
 function Board({ nrows, ncols, chanceLightStartsOn }) {
   const [board, setBoard] = useState(createBoard());
@@ -33,7 +25,13 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
     let initialBoard = [];
-    // TODO: create array-of-arrays of true/false values
+    for (let row = 0; row < nrows; row++) {
+      let rowArray = [];
+      for (let cell = 0; cell < ncols; cell++) {
+        rowArray.push(Math.random() < chanceLightStartsOn);
+      }
+      initialBoard.push(rowArray);
+    }
     return initialBoard;
   }
 
@@ -42,7 +40,7 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   }
 
   function flipCellsAround(coord) {
-    setBoard(oldBoard => {
+    setBoard((oldBoard) => {
       const [y, x] = coord.split("-").map(Number);
 
       const flipCell = (y, x, boardCopy) => {
